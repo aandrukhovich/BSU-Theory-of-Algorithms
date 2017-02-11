101e9b5f2ee693076918b9efb8f7d8e03813ad24
aandrukhovich <pinskblr@gmail.com>
2017-02-11 20:35:35 +0300

    fix make_unique(for c++11) in bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <memory>
 
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+
 template <class T>
 class BST {
   public:
